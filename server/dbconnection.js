e3ab9bf0b8fc8485834e6d48c24a8964dd58d29b
Gautam Dheer <gautam.litesh@gmail.com>
2021-02-14 00:28:26 +0530

    make it better

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -3,8 +3,8 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
-  database: 'btapplication'
+  password: '',
+  // database: 'btapplication'
 })
 
 connection.connect(function (err) {
